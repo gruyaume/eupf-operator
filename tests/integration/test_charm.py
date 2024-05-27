12cb7a9e28e9c3402477d76de98bc1f73903a842
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-05-27 14:40:26 -0400

    chore: fix integration tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -17,18 +17,13 @@ APP_NAME = METADATA["name"]
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest):
-    """Build the charm-under-test and deploy it together with related charms.
+async def test_deploy(ops_test: OpsTest, request):
 
-    Assert on the unit status before any relations/configurations take place.
-    """
-    # Build and deploy charm from local source folder
-    charm = await ops_test.build_charm(".")
+    charm_path = Path(request.config.getoption("--charm_path")).resolve()
     resources = {"httpbin-image": METADATA["resources"]["httpbin-image"]["upstream-source"]}
 
-    # Deploy the charm and wait for active/idle status
     await asyncio.gather(
-        ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME),
+        ops_test.model.deploy(charm_path, resources=resources, application_name=APP_NAME),
         ops_test.model.wait_for_idle(
             apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
         ),
