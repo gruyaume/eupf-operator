2323586febb1774f94c2e75b2d45564cde8f75d4
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-05-27 18:54:30 -0400

    feat: add config file management

--- src/charm.py
+++ src/charm.py
@@ -3,19 +3,49 @@
 # See LICENSE file for licensing details.
 
 
-"""Charm the service.
-
-Refer to the following tutorial that will help you
-develop a new k8s charm using the Operator Framework:
-
-https://juju.is/docs/sdk/create-a-minimal-kubernetes-charm
-"""
+"""Charm the eUPF service."""
 
 import logging
 
 import ops
+import yaml
+from jinja2 import Environment, FileSystemLoader
+from machine import Machine
+from ops import (
+    ActiveStatus,
+    BlockedStatus,
+    CollectStatusEvent,
+    WaitingStatus,
+)
+
+UPF_CONFIG_FILE_NAME = "config.yaml"
+UPF_CONFIG_PATH = "/var/snap/eupf/common"
+PFCP_ADDRESS = "127.0.0.1:8805"
+N3_ADDRESS = "127.0.0.1"
+INTERFACE_NAME = "lo"
 
 logger = logging.getLogger(__name__)
+
+def render_upf_config_file(
+    pfcp_address: str,
+    n3_address: str,
+    interface_name: str,
+) -> str:
+    """Render the configuration file for the 5G UPF service.
+
+    Args:
+        pfcp_address: The PFCP address.
+        n3_address: The N3 address.
+        interface_name: The interface name.
+    """
+    jinja2_environment = Environment(loader=FileSystemLoader("src/templates/"))
+    template = jinja2_environment.get_template(f"{UPF_CONFIG_FILE_NAME}.j2")
+    content = template.render(
+        pfcp_address=pfcp_address,
+        n3_address=n3_address,
+        interface_name=interface_name,
+    )
+    return content
 
 
 class EupfOperatorCharm(ops.CharmBase):
@@ -23,11 +53,50 @@ class EupfOperatorCharm(ops.CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self._machine = Machine()
+        self.framework.observe(self.on.collect_unit_status, self._on_collect_status)
+        self.framework.observe(self.on.config_changed, self._configure)
+
+    def _on_collect_status(self, event: CollectStatusEvent):
+        """Collect unit status."""
+        if not self.unit.is_leader():
+            event.add_status(BlockedStatus("Scaling is not implemented for this charm"))
+            return
+        if not self._upf_config_file_is_written():
+            event.add_status(WaitingStatus("Waiting for UPF configuration file"))
+            return
+        event.add_status(ActiveStatus())
 
 
-    def _on_config_changed(self, event: ops.ConfigChangedEvent):
-        self.unit.status = ops.ActiveStatus()
+    def _configure(self, _):
+        if not self.unit.is_leader():
+            return
+        self._generate_config_file()
+
+    def _generate_config_file(self) -> None:
+        content = render_upf_config_file(
+            pfcp_address=PFCP_ADDRESS,
+            n3_address=N3_ADDRESS,
+            interface_name=INTERFACE_NAME,
+        )
+        if not self._upf_config_file_is_written() or not self._upf_config_file_content_matches(
+            content=content
+        ):
+            self._write_upf_config_file(content=content)
+
+    def _upf_config_file_is_written(self) -> bool:
+        return self._machine.exists(path=f"{UPF_CONFIG_PATH}/{UPF_CONFIG_FILE_NAME}")
+
+    def _upf_config_file_content_matches(self, content: str) -> bool:
+        existing_content = self._machine.pull(path=f"{UPF_CONFIG_PATH}/{UPF_CONFIG_FILE_NAME}")
+        try:
+            return yaml.safe_load(existing_content) == yaml.safe_load(content)
+        except yaml.YAMLError:
+            return False
+
+    def _write_upf_config_file(self, content: str) -> None:
+        self._machine.push(path=f"{UPF_CONFIG_PATH}/{UPF_CONFIG_FILE_NAME}", source=content)
+        logger.info("Pushed %s config file", UPF_CONFIG_FILE_NAME)
 
 
 
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,18 +1,68 @@
 # Copyright 2024 Guillaume Belanger
 # See LICENSE file for licensing details.
 
-import unittest
 
-import ops
-import ops.testing
+from unittest.mock import MagicMock, patch
+
+import pytest
+import yaml
 from charm import EupfOperatorCharm
+from ops import ActiveStatus, WaitingStatus, testing
+
+NAMESPACE = "whatever"
+
+def read_file(path: str) -> str:
+    """Read a file and returns as a string."""
+    with open(path, "r") as f:
+        content = f.read()
+    return content
 
 
-class TestCharm(unittest.TestCase):
-    def setUp(self):
-        self.harness = ops.testing.Harness(EupfOperatorCharm)
-        self.addCleanup(self.harness.cleanup)
+class TestCharm:
+    patcher_machine = patch("charm.Machine")
+
+    @pytest.fixture()
+    def setup(self):
+        self.mock_machine = MagicMock()
+        self.mock_machine.pull.return_value = ""
+        mock_machine = TestCharm.patcher_machine.start()
+        mock_machine.return_value = self.mock_machine
+
+    @pytest.fixture(autouse=True)
+    def harness(self, setup, request):
+        self.harness = testing.Harness(EupfOperatorCharm)
+        self.harness.set_model_name(name=NAMESPACE)
+        self.harness.set_leader(is_leader=True)
         self.harness.begin()
+        yield self.harness
+        self.harness.cleanup()
+        request.addfinalizer(self.teardown)
 
-    def test_httpbin_pebble_ready(self):
-        self.assertEqual(1, 1)
+    @staticmethod
+    def teardown() -> None:
+        patch.stopall()
+
+    def test_given_fiveg_config_file_not_created_when_evaluate_status_then_status_is_waiting(
+        self
+    ):
+        self.mock_machine.exists.return_value = False
+        self.harness.evaluate_status()
+
+        assert self.harness.model.unit.status == WaitingStatus("Waiting for UPF configuration file")
+
+    def test_given_config_file_created_when_evaluate_status_then_status_is_active(self):
+        self.mock_machine.exists.return_value = True
+        self.harness.evaluate_status()
+
+        assert self.harness.model.unit.status == ActiveStatus()
+
+    def test_given_config_file_not_created_when_config_changed_then_file_created(self):
+        self.mock_machine.exists.return_value = False
+
+        self.harness.update_config()
+
+        expected_config_file_content = read_file("tests/unit/expected_config.yaml").strip()
+
+        _, kwargs = self.mock_machine.push.call_args
+        assert kwargs["path"] == "/var/snap/eupf/common/config.yaml"
+        assert yaml.safe_load(kwargs["source"]) == yaml.safe_load(expected_config_file_content)
